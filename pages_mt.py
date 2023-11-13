db3ef30decc4448e789ec107ae9e69ebf8c88f07
bongry <141402618+bongrybongbong@users.noreply.github.com>
2023-11-13 09:35:09 +0900

    Add files via upload

---
+++ pages_mt.py
@@ -0,0 +1,50 @@
+page_data = {
+    "page1": {
+        "title": "ThinkWide 프로젝트 회의록",
+        "description": "ThinkWide 가상 공간 설계, 마케팅 전략 수립에 대해 구체적인 작업 방향을 설정했어요!",
+        "keywords": ["비즈니스 모델", "사용자 경험", "UI/UX디자인"],
+        'img_file': r'C:\CODE\thinkwide_app-main\img\page1.webp'
+    },
+    "page2": {
+        "title": "두근두근 가족 여행",
+        "description": "다가올 마케팅 캠페인, 디지털 아웃리치, 소셜 미디어 참여 향상 전략에 대해 논의했어요!",
+        "keywords": ["여행지 선택", "운전자 뽑기", "여행 날짜 맞추기"],
+        'img_file': r'C:\CODE\thinkwide_app-main\img\page2.webp'
+    },
+    "page3": {
+        "title": "귀여운 우리집 강아지 생일",
+        "description": "귀여운 우리집 강아지의 생일 날 파티를 구상했어요!.",
+        "keywords": ["강아지 케이크 주문", "초대 손님", "개파티"],
+        'img_file': r'C:\CODE\thinkwide_app-main\img\page3.webp'
+    },
+    "page4": {
+        "title": "일본 여행 계획 짜기",
+        "description": "친구랑 같이 한달 뒤 일본 여행에 대해 논의했어요",
+        "keywords": ["날짜 픽스", "어디로?", "지겨워"],
+        'img_file': r'C:\CODE\thinkwide_app-main\img\page4.webp'
+    },
+    "page5": {
+        "title": "기술 로드맵 계획",
+        "description": "팀원들과 다가오는 기술 이니셔티브, IT 프로젝트 우선 순위 결정, 그리고 다음 분기에 걸친 새로운 시스템 통합에 대해 논의했어요!",
+        "keywords": ["기술 이니셔티브", "IT 프로젝트", "시스템 통합"],
+        'img_file': r'C:\CODE\thinkwide_app-main\img\page5.webp'
+    },
+        "page6": {
+        "title": "여행 계획에 대한 대화",
+        "description": "친구들과 다가오는 여름 휴가를 위한 여행지 선택, 숙소 예약, 그리고 활동 계획을 이야기 나누며 공유했어요!",
+        "keywords": ["여행지 선택", "숙소 예약", "활동 계획"],
+        'img_file': r'C:\CODE\thinkwide_app-main\img\page6.webp'
+    },
+    "page7": {
+        "title": "취미 생활 공유",
+        "description": "새로 시작한 취미에 대한 경험을 나누고, 서로의 취미 생활에서 영감을 주고받는 대화를 나눴어요!",
+        "keywords": ["취미 생활", "경험 공유", "영감"],
+        'img_file': r'C:\CODE\thinkwide_app-main\img\page7.webp'
+    },
+    "page8": {
+        "title": "건강한 생활 습관",
+        "description": "건강한 식습관, 운동 루틴, 그리고 스트레스 관리 방법에 대해 이야기하며, 서로의 생활 습관을 개선하기 위한 팁을 교환했어요!",
+        "keywords": ["건강한 식습관", "운동 루틴", "스트레스 관리"],
+        'img_file': r'C:\CODE\thinkwide_app-main\img\page8.webp'
+    }
+}
